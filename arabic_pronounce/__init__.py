bdd65796e3e3922253945a95064778612d32c0d3
Youssef Sherif <sharief@aucegypt.edu>
2018-08-21 23:43:30 +0200

    Fix bug

--- arabic_pronounce/__init__.py
+++ arabic_pronounce/__init__.py
@@ -367,7 +367,7 @@ def phonetise(text):
 					iterations = 1
 					for index, letter in enumerate(phones):
 						if(isinstance(letter, list)):
-							curIndex = (i / iterations) % len(letter)
+							curIndex = (i // iterations) % len(letter)
 							if(letter[curIndex] != u''):
 								pronunciations[-1].append(letter[curIndex])
 							iterations = iterations * len(letter)
